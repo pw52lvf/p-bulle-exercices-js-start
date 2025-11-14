a11e3920a1c6cf1dd2ec99b1ffc31591c85d4e73
pw52lvf <pw52lvf@eduvaud.ch>
2025-11-14 15:35:22 +0100

    feat(poetry-club-door-policy) Exercice door-policy terminé [30] [DONE]

--- exercices/poetry-club-door-policy/door-policy.js
+++ exercices/poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line[0];
 }
 
 /**
@@ -38,7 +38,11 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  let restoftheword = '';
+  for (let i = 1; i < word.length; i++){
+    restoftheword += word[i].toLowerCase();
+  }
+  return word[0].toUpperCase() + restoftheword;
 }
 
 /**
@@ -49,7 +53,9 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  let newline = line.trim();
+  let indexoflastletter = newline.length - 1;
+  return newline[indexoflastletter];
 }
 
 /**
@@ -60,5 +66,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return frontDoorPassword(word) + ", please"
 }
