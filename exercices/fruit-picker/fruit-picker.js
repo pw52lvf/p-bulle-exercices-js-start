bc33c409a46ce03ab7a4f1a03a47077d35aee181
pw52lvf <pw52lvf@eduvaud.ch>
2025-12-12 14:29:59 +0100

    feat(fruit-picker): Exercice fruit-picker terminé [DONE] [10]

--- exercices/fruit-picker/fruit-picker.js
+++ exercices/fruit-picker/fruit-picker.js
@@ -33,7 +33,7 @@ export function onError() {
  * @return void
  */
 export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
-  return order(query)
+  return order(query, onSuccessCallback, onErrorCallback)
 }
 
 /**
@@ -42,5 +42,5 @@ export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
  * @return void
  */
 export function postOrder(variety, quantity) {
-  return 'order placed for ' + quantity + ' ' + variety
+  return order({ variety, quantity }, onSuccess, onError)
 }
