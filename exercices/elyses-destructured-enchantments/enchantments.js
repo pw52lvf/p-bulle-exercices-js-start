302a913d4a3b74c47cc9edeccfe6b83217fce93c
pw52lvf <pw52lvf@eduvaud.ch>
2025-12-13 14:13:47 +0100

    feat(elyses-destructured-enchantments): exercice elyses-destructured-enchantments terminé [DONE] [40]

--- exercices/elyses-destructured-enchantments/enchantments.js
+++ exercices/elyses-destructured-enchantments/enchantments.js
@@ -9,7 +9,8 @@
  * @returns {Card} the first card in the deck
  */
 export function getFirstCard(deck) {
-  throw new Error('Implement the getFirstCard function');
+  const [firstcard] = deck;
+  return firstcard;
 }
 
 /**
@@ -20,7 +21,8 @@ export function getFirstCard(deck) {
  * @returns {Card} the second card in the deck
  */
 export function getSecondCard(deck) {
-  throw new Error('Implement the getSecondCard function');
+  const [firstcard, secondcard] = deck;
+  return secondcard;
 }
 
 /**
@@ -31,7 +33,8 @@ export function getSecondCard(deck) {
  * @returns {[Card, Card]} new deck with the 2 cards swapped
  */
 export function swapTwoCards(deck) {
-  throw new Error('Implement the swapTopTwoCards function');
+  const [firstcard, secondcard] = deck;
+  return [secondcard, firstcard];
 }
 
 /**
@@ -42,7 +45,8 @@ export function swapTwoCards(deck) {
  * @returns {[Card, Card, Card]} new deck with the 3 cards shifted by one position
  */
 export function shiftThreeCardsAround(deck) {
-  throw new Error('Implement the discardTopCard function');
+  const [firstcard, secondcard, thirdcard] = deck;
+  return [secondcard, thirdcard, firstcard];
 }
 
 /**
@@ -54,7 +58,8 @@ export function shiftThreeCardsAround(deck) {
  */
 export function pickNamedPile(piles) {
   // 🚨 Do NOT use piles.chosen or piles.disregarded.
-  throw new Error('Implement the pickNamedPile function');
+  const { chosen, disregarded } = piles
+  return chosen;
 }
 
 /**
@@ -67,5 +72,6 @@ export function swapNamedPile(piles) {
   // 🪄 Don't break the magic.
   // 🚨 Do NOT use piles.chosen or piles.disregarded.
   // 🚨 Do NOT touch the next line or Elyse will accidentally reveal the trick.
+  const { disregarded: chosen, chosen: disregarded} = piles;
   return { chosen, disregarded };
 }
