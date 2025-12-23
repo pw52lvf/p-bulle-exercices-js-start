acce4fb360f3eda630265edf3136f67f2f785899
pw52lvf <pw52lvf@eduvaud.ch>
2025-12-23 18:30:11 +0100

    feat(parallel-letter-frequency): Exercice parallel-letter-frequency terminé [DONE] [90]

--- exercices/parallel-letter-frequency/parallel-letter-frequency.js
+++ exercices/parallel-letter-frequency/parallel-letter-frequency.js
@@ -1,9 +1,25 @@
 //
-// This is only a SKELETON file for the 'Parallel Letter Frequency' exercise. It's been provided as a
+// This is only a SKELETON file for the "Parallel Letter Frequency" exercise. It"s been provided as a
 // convenience to get you started writing code faster.
 //
 
 export const parallelLetterFrequency = async (texts) => {
-  let letters = {  }
+  let acceptables = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ø", "φ", "ほ", "本"]
+  let letters = {};
+  let text = texts.join("")
+  let characters = text.toLowerCase().split("").sort();
+  let num = 1;
+  for (let i = 0; i < characters.length; i++){
+    if (acceptables.includes(characters[i])){
+      if (characters[i] === characters[i + 1]){
+      num++;
+    }
+    else {
+      letters[characters[i]] = num;
+      num = 1; 
+    }
+    }
+  }
   
+  return letters;
 };
