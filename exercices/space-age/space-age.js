ec8ac0d2513850d4b4acc6367d3df97437646102
pw52lvf <pw52lvf@eduvaud.ch>
2025-12-30 17:00:30 +0100

    feat(space-age): Exercice space-age terminé [DONE] [30]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,29 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = (planet, seconds) => {
+  switch (planet) {
+    case 'mercury':
+      return calculation(0.2408467, seconds);
+    case 'venus':
+      return calculation(0.61519726, seconds);
+    case 'earth':
+      return calculation(1, seconds);
+    case 'mars':
+      return calculation(1.8808158, seconds);
+    case 'jupiter':
+      return calculation(11.862615, seconds);
+    case 'saturn':
+      return calculation(29.447498, seconds);
+    case 'uranus':
+      return calculation(84.016846, seconds);
+    case 'neptune':
+      return calculation(164.79132, seconds);
+    default:
+      throw new Error("not a planet");
+  }
 };
+
+const calculation = (num, seconds) => {
+    return Math.round((seconds / 31557600 / num) * 100) / 100;
+}
