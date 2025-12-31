fd08760eba2b126c8a947453c29725dc4a1785d4
pw52lvf <pw52lvf@eduvaud.ch>
2025-12-31 14:42:06 +0100

    feat(custom-signs): Exercice custom-signs terminé [DONE] [15]

--- exercices/custom-signs/custom-signs.js
+++ exercices/custom-signs/custom-signs.js
@@ -14,7 +14,7 @@
  */
 
 export function buildSign(occasion, name) {
-  throw new Error('Remove this line and implement the function');
+  return `Happy ${occasion} ${name}!`
 }
 
 /**
@@ -26,7 +26,7 @@ export function buildSign(occasion, name) {
  */
 
 export function buildBirthdaySign(age) {
-  throw new Error('Remove this line and implement the function');
+  return `Happy Birthday! What a ${age >= 50 ? 'mature' : 'young'} fellow you are.`
 }
 
 /**
@@ -39,7 +39,7 @@ export function buildBirthdaySign(age) {
  */
 
 export function graduationFor(name, year) {
-  throw new Error('Remove this line and implement the function');
+  return `Congratulations ${name}!\nClass of ${year}`
 }
 
 /**
@@ -53,5 +53,5 @@ export function graduationFor(name, year) {
  */
 
 export function costOf(sign, currency) {
-  throw new Error('Remove this line and implement the function');
+  return `Your sign costs ${sign.length * 2 + 20}.00 ${currency}.`
 }
